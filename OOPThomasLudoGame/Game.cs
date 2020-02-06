5433240a8a5b86d809bac69c0aaaf8212f8c5b50
Bakerling25 <thom97m6@edu.sde.dk>
2020-02-06 14:41:25 +0100

    LudoGame V2

---
+++ OOPThomasLudoGame/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPThomasLudoGame
+{
+    class Game
+    {
+        private int numberOfPlayers;
+        public int NumberOfPlayers
+        { 
+            get { return numberOfPlayers; }
+            set { numberOfPlayers = NumberOfPlayers; }
+        }
+        List<string> names = new List<string>();
+        List<Player> players = new List<Player>();
+        PlayerCreator playerCreator = new PlayerCreator();
+        public void Play()
+        {
+            players = playerCreator.PlayerCreating(NamePlayerList(NumberOfPlayers), PieceCreator.CreatePiece());
+        }
+        public List<string> NamePlayerList(int numberOfPlayer)
+        {
+            string name;
+            for (int i = 0; i < numberOfPlayer; i++)
+            {
+                Console.WriteLine("Skriv navnet på spiller " + i);
+                name = Console.ReadLine();
+                names.Add(name);
+            }
+            return names;
+        }
+        
+    }
+}
